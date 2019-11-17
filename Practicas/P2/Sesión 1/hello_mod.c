1a58edf41d4465413a48a931ad0d4f9c0b1e754f
Nazaret <nazaretrogue@correo.ugr.es>
2019-11-17 18:19:40 +0100

    Añadidos fuentes

---
+++ Practicas/P2/Sesión 1/hello_mod.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+	printf("Hello world!\n");
+	sleep(1000);
+
+	return 0;
+}
---
+++ Practicas/P2/Sesión 1/hello_mod.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main(){
+	cout << "Hello world!" << endl;
+	sleep(1000);
+	return 0;
+}
